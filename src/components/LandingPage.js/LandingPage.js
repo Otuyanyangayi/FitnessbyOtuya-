6daac2b5d8950bc13b4ed6caa202f11d8db4bbe2
Grace <gracenyangayi@gmail.com>
2022-08-26 15:37:47 +0300

    add LandingPage component

---
+++ src/components/LandingPage.js/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./LandingPage.css"
+
+function LandingPage(){
+    return(
+        <div className="LandingPage">
+            <button className="button">My Routines</button>
+
+            <span className="span">Fitness By Otuya</span>
+        </div>
+    )
+}
+
+export default LandingPage
