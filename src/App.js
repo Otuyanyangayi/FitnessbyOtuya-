320e859e4b4bc62247b4e296dfc3394501799113
Grace <gracenyangayi@gmail.com>
2022-08-26 15:41:18 +0300

    display Landing page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Container, withStyles,Switch } from "@material-ui/core";
 import { grey } from "@material-ui/core/colors";
 import React, {  useState } from "react";
 import Header from "./components/Header/Header"
+import LandingPage from "./components/LandingPage.js/LandingPage";
+import {Router} from "react-router-dom";
 
 import './App.css';
 
@@ -30,16 +32,15 @@ function App() {
     <div style={{ backgroundColor:LightMode ? "gold" : "black", backgroundRepeat: 'no-repeat', backgroundsize:"cover",
     color: LightMode ? "black" : "white"}}>
       <Container maxWidth="md" style={{display: "flex", flexDirection:"column", height : "100vh"}}>
+      
       <Header/>
-      <div
-          style={{ position: "absolute", top: 0, right: 15, paddingTop: 10 }}
-        >
+
+      <div style={{ position: "absolute", top: 0, right: 15, paddingTop: 10 }}>
           <span>{LightMode ? "Dark" : "Light"} Mode</span>
-          <DarkMode 
-            checked={LightMode}
-            onChange={() => setLightMode(!LightMode)}
-          />
+          <DarkMode checked={LightMode} onChange={() => setLightMode(!LightMode)}/>
         </div>
+        <LandingPage/>
+        
         
       </Container>
     </div>
