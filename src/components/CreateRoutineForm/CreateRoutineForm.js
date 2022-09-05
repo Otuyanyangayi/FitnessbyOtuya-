b88c2e294969c918d24a0891f16e0b0e8d9b5ca5
Grace <gracenyangayi@gmail.com>
2022-09-05 11:22:23 +0300

    Create routine form

---
+++ src/components/CreateRoutineForm/CreateRoutineForm.js
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import Header from "../Header/Header";
+import './CreateRoutineForm.css';
+
+function CreateRoutineForm() {
+  const [formData, setFormData] = useState({
+    routine_name: "",
+    muscle_group: "",
+    personal_trainer_id: "",
+    routine_image: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    reset();
+  };
+
+  const reset = () => {
+    setFormData({
+      routine_name: "",
+      muscle_group: "",
+      personal_trainer_id: "",
+      routine_image: "",
+    });
+  };
+
+  return (
+    <>
+      <Header />
+      <div className="layout">
+        <form onSubmit={handleSubmit}>
+          <div className="layoutdiv">
+            <label htmlFor="RoutineName"> Routine Name: </label>
+            <input
+              id="RoutineName"
+              type="text"
+              name="routine_name"
+              placeholder="Routine name here..."
+              value={formData.routine_name}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="layoutdiv">
+            <label htmlFor="MuscleGroup"> Muscle Group: </label>
+            <input
+              type="text"
+              id="MuscleGroup"
+              name="muscle_group"
+              placeholder="Muscle Group here..."
+              value={formData.muscle_group}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="layoutdiv">
+            <label htmlFor="PersonalTrainer"> Personal Trainer ID: </label>
+            <input
+              type="number"
+              id="PersonalTrainerID"
+              name="personal_trainer_id"
+              placeholder="Personal Trainer ID here..."
+              value={formData.personal_trainer_id}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="layoutdiv">
+            <label htmlFor="image"> Picture URL: </label>
+            <input
+              id="imageInput"
+              type="url"
+              name="routine_image"
+              placeholder="Image here..."
+              value={formData.routine_image}
+              onChange={handleChange}
+            />
+          </div>
+          <button className="layout-btn" type="reset" onClick={handleSubmit}>
+            Add Routine{" "}
+          </button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default CreateRoutineForm;
