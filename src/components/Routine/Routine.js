e11bb0b415d907ff84ecf95d966e32981e6277aa
Grace <gracenyangayi@gmail.com>
2022-09-05 11:57:50 +0300

    create routine component

---
+++ src/components/Routine/Routine.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./Routine.css"
+
+function Routine(){
+
+    return(
+        <div className="routine-box">
+            <h3 style={{fontSize: "1.30em" }}>Example name</h3>
+            <div className="routine-content">
+                <li>Routine ID: 30</li>
+                Focus: example
+            </div>
+            <button className='routine-btn'>
+                Details
+            </button>
+            <button className='routine-btn'>Delete</button>
+        
+        </div>
+
+    )
+}
+
+export default Routine
