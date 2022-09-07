6e02d3e6ad77b9ba99bda9a53264cfdfcdca2d68
Grace <gracenyangayi@gmail.com>
2022-09-07 15:21:44 +0300

    update routines component

--- src/components/Routine/Routine.js
+++ src/components/Routine/Routine.js
@@ -1,37 +1,27 @@
 import React from 'react';
 import "./Routine.css"
 
-function Routine( isTrue){
+function Routine({routine, handleDelete, displayDetails, displayRoutineImage, isTrue}){
 
     return(
         <>
         <div className="routine-box">
-            <h3 style={{fontSize: "1.30em" }}>Example name</h3>
+            <h3 style={{fontSize: "1.30em" }}>{routine.routine_name}</h3>
             <div className="routine-content">
-                <li>Routine ID: 30</li>
-                Focus: example
+                <li>Routine ID: {routine.id}</li>
+                Focus: {routine.muscle_group}
             </div>
             <button className='routine-btn' onClick={() => {
                 isTrue()
-               
-            }}>
+                displayRoutineImage(routine.routine_image)
+                displayDetails(routine.exercises)
+               }}>
                 Details
             </button>
-            <button className='routine-btn'>Delete</button>
+            <button className='routine-btn' onClick={() => handleDelete(routine.id)}>Delete</button>
         
         </div>
-        <div className="routine-box">
-            <h3 style={{fontSize: "1.30em" }}>Example name</h3>
-            <div className="routine-content">
-                <li>Routine ID: 30</li>
-                Focus: example
-            </div>
-            <button className='routine-btn'>
-                Details
-            </button>
-            <button className='routine-btn'>Delete</button>
         
-        </div>
         
 
         </>
