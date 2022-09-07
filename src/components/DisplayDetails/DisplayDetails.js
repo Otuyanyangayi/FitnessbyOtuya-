fd018ea587aac687e71cfe99d5e2a48a7a041bb0
Grace <gracenyangayi@gmail.com>
2022-09-07 15:18:39 +0300

    update desplay details

--- src/components/DisplayDetails/DisplayDetails.js
+++ src/components/DisplayDetails/DisplayDetails.js
@@ -1,34 +1,37 @@
-import React from 'react';
-import "./DisplayDetails.css"
+import React from "react";
+import "./DisplayDetails.css";
 
-
-function DisplayDetails () {
-    
-    return (
-        
-        <div className="display-layout">
-            <div>
-                
-                <>
-                <p style={{ marginTop: "17rem"}}>Click on a routine for more details</p><p>| 💪 | 🏋️ | 🏃‍♀️ |</p>
-                </>
-                
+function DisplayDetails({ routineExercises, routineImage }) {
+  return (
+    <div className="display-layout">
+      <div>
+        {routineImage ? (
+          <img
+            style={{ margin: "2rem" }}
+            alt="routineImage"
+            src={routineImage}
+          />
+        ) : (
+          <>
+            <p style={{ marginTop: "20rem" }}>
+              Click on a routine for more details
+            </p>
+            <p>| 💪 | 🏋️ | 🏃‍♀️ |</p>
+          </>
+        )}
+      </div>
+      <div>
+        {routineExercises.map((exercise) => (
+          <>
+            <div className="exercise-box">
+              <p>Exercise Name: {exercise.name}</p>
+              <p>Instructions: {exercise.instructions}</p>
             </div>
-            <div>
-                
-                    <>
-                        <div className="exercise-box">
-                            <p>Exercise Name: exercise.name</p>
-                            <p>Instructions: exercise.instructions</p>
-                        </div>
-                    </> 
-                
-                
-            </div>
-        </div>
-
-    );
+          </>
+        ))}
+      </div>
+    </div>
+  );
 }
-
 
 export default DisplayDetails;
